c41d40aea81c07749df334a3bfd380b3ca11e7d4
Daniel Demonteverde <demonteverde@themont.local>
2019-06-28 12:15:49 +0200

    added parallax effect video element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 document.getElementById('btnPreProduction').addEventListener('click', function(){
   window.location.href = './pre-production.html'
 })
+
+window.onscroll = function(e){
+  console.log(this.pageYOffset)
+  console.log(this.scrollY)
+
+  var scrolled = window.pageYOffset
+  document.getElementsByClassName('video-element')[0].style.transform = 'translateY('+scrolled * -2+'px)'
+}
