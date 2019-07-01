c787c42d50d308b0864d57ae1834a4e27c52044e
Daniel Demonteverde <demonteverde@themont.local>
2019-07-01 11:54:02 +0200

    edited index js nebbia srcs

--- js/index.js
+++ js/index.js
@@ -12,22 +12,22 @@ let currentFrame = 1
 //   document.getElementsByClassName('video-element')[0].style.transform = 'translateY('+scrolled * -2+'px)'
 // }
 
-// window.onload = function(e){
-//   setInterval(function(){
-//     changeFrame()
-//   }, 100)
-// }
+window.onload = function(e){
+  setInterval(function(){
+    changeFrame()
+  }, 100)
+}
 
 function changeFrame(){
 
     if(currentFrame == 96){
       currentFrame = 0
       console.log( document.getElementById('slotNebbia') )
-      document.getElementById('slotNebbia').src = '/images/GifNebbia/nebbia_'+currentFrame+'.png'
+      document.getElementById('slotNebbia').src = '/drowned-website/images/GifNebbia/nebbia_'+currentFrame+'.png'
     }
     else{
       currentFrame++
       console.log( document.getElementById('slotNebbia') )
-      document.getElementById('slotNebbia').src = '/images/GifNebbia/nebbia_'+currentFrame+'.png'
+      document.getElementById('slotNebbia').src = '/drowned-website/images/GifNebbia/nebbia_'+currentFrame+'.png'
     }
 }
