f282838cd2bc04ad537402cfbb8dbac0222ad0f4
Daniel Demonteverde <demonteverde@themont.local>
2019-07-01 11:14:21 +0200

    removed extra features

--- js/index.js
+++ js/index.js
@@ -1,11 +1,33 @@
-document.getElementById('btnPreProduction').addEventListener('click', function(){
-  window.location.href = './pre-production.html'
-})
+// document.getElementById('btnPreProduction').addEventListener('click', function(){
+//   window.location.href = './pre-production.html'
+// })
 
-window.onscroll = function(e){
-  console.log(this.pageYOffset)
-  console.log(this.scrollY)
+let currentFrame = 1
 
-  var scrolled = window.pageYOffset
-  document.getElementsByClassName('video-element')[0].style.transform = 'translateY('+scrolled * -3+'px)'
+// window.onscroll = function(e){
+//   console.log(this.pageYOffset)
+//   console.log(this.scrollY)
+
+//   var scrolled = window.pageYOffset
+//   document.getElementsByClassName('video-element')[0].style.transform = 'translateY('+scrolled * -2+'px)'
+// }
+
+// window.onload = function(e){
+//   setInterval(function(){
+//     changeFrame()
+//   }, 100)
+// }
+
+function changeFrame(){
+
+    if(currentFrame == 96){
+      currentFrame = 0
+      console.log( document.getElementById('slotNebbia') )
+      document.getElementById('slotNebbia').src = '/images/GifNebbia/nebbia_'+currentFrame+'.png'
+    }
+    else{
+      currentFrame++
+      console.log( document.getElementById('slotNebbia') )
+      document.getElementById('slotNebbia').src = '/images/GifNebbia/nebbia_'+currentFrame+'.png'
+    }
 }
