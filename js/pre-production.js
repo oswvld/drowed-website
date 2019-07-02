5d7911622aadf7471005e80fee424dfe4e4cfdb9
Daniel Demonteverde <demonteverde@themont.local>
2019-07-02 10:50:33 +0200

    added pre production js code

--- js/pre-production.js
+++ js/pre-production.js
@@ -1,3 +1,4 @@
+/* Concept */
 let conceptSectionBlock = document.getElementById('conceptSection')
 let conceptSectionHiddenBlock = document.getElementById('conceptSection-hidden')
 
@@ -9,4 +10,49 @@ conceptSectionBlock.onclick = function() {
     conceptSectionHiddenBlock.style.display = 'none'
   else
     conceptSectionHiddenBlock.style.display = 'block'
+}
+
+/* Personaggio */
+
+let personaggioSectionBlock = document.getElementById('personaggioSection')
+let personaggioSectionHiddenBlock = document.getElementById('personaggioSection-hidden')
+
+personaggioSectionBlock.onclick = function() {
+
+  console.log(personaggioSectionHiddenBlock.style.display)
+
+  if( personaggioSectionHiddenBlock.style.display == 'block' )
+  personaggioSectionHiddenBlock.style.display = 'none'
+  else
+  personaggioSectionHiddenBlock.style.display = 'block'
+}
+
+/* Ambiente */
+
+let ambienteSectionBlock = document.getElementById('ambienteSection')
+let ambienteSectionHiddenBlock = document.getElementById('ambienteSection-hidden')
+
+ambienteSectionBlock.onclick = function() {
+
+  console.log(ambienteSectionHiddenBlock.style.display)
+
+  if( ambienteSectionHiddenBlock.style.display == 'block' )
+  ambienteSectionHiddenBlock.style.display = 'none'
+  else
+  ambienteSectionHiddenBlock.style.display = 'block'
+}
+
+/* Storyboard */
+
+let storyboardSectionBlock = document.getElementById('storyboardSection')
+let storyboardSectionHiddenBlock = document.getElementById('storyboardSection-hidden')
+
+storyboardSectionBlock.onclick = function() {
+
+  console.log(storyboardSectionHiddenBlock.style.display)
+
+  if( storyboardSectionHiddenBlock.style.display == 'block' )
+  storyboardSectionHiddenBlock.style.display = 'none'
+  else
+  storyboardSectionHiddenBlock.style.display = 'block'
 }
