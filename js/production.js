0d82631d54e67aaf8e8d469a6a24be3a356036d9
Daniel Demonteverde <demonteverde@themont.local>
2019-07-02 14:59:03 +0200

    added production js

---
+++ js/production.js
@@ -0,0 +1,87 @@
+/* Personaggio */
+let personaggioSectionBlock = document.getElementById('production_personaggioSection')
+let personaggioSectionHiddenBlock = document.getElementById('production_personaggioSection-hidden')
+
+personaggioSectionBlock.onclick = function() {
+  if( personaggioSectionHiddenBlock.style.display == 'block' )
+    personaggioSectionHiddenBlock.style.display = 'none'
+  else
+    personaggioSectionHiddenBlock.style.display = 'block'
+}
+
+/* Path */
+let pathSectionBlock = document.getElementById('production_pathSection')
+let pathSectionHiddenBlock = document.getElementById('production_pathSection-hidden')
+
+pathSectionBlock.onclick = function() {
+  if( pathSectionHiddenBlock.style.display == 'block' )
+    pathSectionHiddenBlock.style.display = 'none'
+  else
+    pathSectionHiddenBlock.style.display = 'block'
+}
+
+/* Acqua */
+let acquaSectionBlock = document.getElementById('production_acquaSection')
+let acquaSectionHiddenBlock = document.getElementById('production_acquaSection-hidden')
+
+acquaSectionBlock.onclick = function() {
+  if( acquaSectionHiddenBlock.style.display == 'block' )
+    acquaSectionHiddenBlock.style.display = 'none'
+  else
+    acquaSectionHiddenBlock.style.display = 'block'
+}
+
+/* Titolo */
+let titoloSectionBlock = document.getElementById('production_titoloSection')
+let titoloSectionHiddenBlock = document.getElementById('production_titoloSection-hidden')
+
+titoloSectionBlock.onclick = function() {
+  if( titoloSectionHiddenBlock.style.display == 'block' )
+    titoloSectionHiddenBlock.style.display = 'none'
+  else
+    titoloSectionHiddenBlock.style.display = 'block'
+}
+
+/* Cuore */
+let cuoreSectionBlock = document.getElementById('production_cuoreSection')
+let cuoreSectionHiddenBlock = document.getElementById('production_cuoreSection-hidden')
+
+cuoreSectionBlock.onclick = function() {
+  if( cuoreSectionHiddenBlock.style.display == 'block' )
+    cuoreSectionHiddenBlock.style.display = 'none'
+  else
+    cuoreSectionHiddenBlock.style.display = 'block'
+}
+
+/* Lacrima */
+let lacrimaSectionBlock = document.getElementById('production_lacrimaSection')
+let lacrimaSectionHiddenBlock = document.getElementById('production_lacrimaSection-hidden')
+
+lacrimaSectionBlock.onclick = function() {
+  if( lacrimaSectionHiddenBlock.style.display == 'block' )
+    lacrimaSectionHiddenBlock.style.display = 'none'
+  else
+    lacrimaSectionHiddenBlock.style.display = 'block'
+}
+
+/* Cerchio Energia */
+let cerchioSectionBlock = document.getElementById('production_cerchioSection')
+let cerchioSectionHiddenBlock = document.getElementById('production_cerchioSection-hidden')
+
+cerchioSectionBlock.onclick = function() {
+  if( cerchioSectionHiddenBlock.style.display == 'block' )
+    cerchioSectionHiddenBlock.style.display = 'none'
+  else
+    cerchioSectionHiddenBlock.style.display = 'block'
+}
+
+/* Rendering */
+let renderingSectionBlock = document.getElementById('production_renderingSection')
+let renderingSectionHiddenBlock = document.getElementById('production_renderingSection-hidden')
+
+renderingSectionBlock.onclick = function() {
+  if( renderingSectionHiddenBlock.style.display == 'block' )
+    renderingSectionHiddenBlock.style.display = 'none'
+  else
+    renderingSectionHiddenBlock.style.display = 'block'
+}
